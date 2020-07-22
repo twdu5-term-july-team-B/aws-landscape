4010f8c091425006dff1dc0327b0188c18887019
kmok <kmok@thoughtworks.com>
2020-07-22 14:55:16 +0200

    tp/ln/ad/km: adding dimensions for cloudwatch metric alarm for kafka

--- modules/training_kafka/cloudwatch.tf
+++ modules/training_kafka/cloudwatch.tf
@@ -6,9 +6,12 @@ resource "aws_cloudwatch_metric_alarm" "disk_space_utilization" {
   namespace                 = "Linux System"
   period                    = "120"
   statistic                 = "Average"
-  threshold                 = "80"
-  alarm_description         = "This metric sends alarm when the disk utilization is more than 80%"
+  threshold                 = "90"
+  alarm_description         = "This metric sends alarm when the disk utilization is more than 90%"
   insufficient_data_actions = []
   ok_actions = ["${var.alerting_sns_topic}"]
   alarm_actions = ["${var.alerting_sns_topic}"]
+  dimensions = {
+    InstanceId = "${aws_instance.kafka.id}"
+  }
 }
