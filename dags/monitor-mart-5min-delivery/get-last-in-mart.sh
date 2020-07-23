ace5ac84de92d2115e7870add9357047c99f3042
kmok <kmok@thoughtworks.com>
2020-07-23 16:43:03 +0200

    ad/ln/km: cleaning up python script

--- dags/monitor-mart-5min-delivery/get-last-in-mart.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-#echo $(hdfs dfs -ls /tw/stationMart/data | tr -s ' ' | cut -d' ' -f6-7 | grep '^[0-9]' | head -n 1)
-
-
--- dags/monitor-mart-5min-delivery/monitor-mart-5min-delivery.py
+++ dags/monitor-mart-5min-delivery/monitor-mart-5min-delivery.py
@@ -1,9 +1,6 @@
 from airflow import DAG
 from datetime import datetime, timedelta
-from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
-from airflow.contrib.operators.emr_add_steps_operator import EmrAddStepsOperator
-from airflow.contrib.sensors.emr_step_sensor import EmrStepSensor
 
 
 app_name = "monitor-mart-5-min-delivery"
@@ -21,65 +18,14 @@ dag = DAG(
     max_active_runs = 1
 )
 
-def issue_step(name, args):
-  return [
-    {
-      "Name": name,
-      "ActionOnFailure": "CONTINUE",
-      "HadoopJarStep": {
-        "Jar": "command-runner.jar",
-        "Args": args
-      }
-    }
-  ]
+def some_function() :
+  return "foo"
 
-HDFS -> bash file
-   gets the date `hdfs dfs -ls .... ` => 2020-07-23 09:30
-  python script ^^ compares with 5 min ago
-  return 0 if success
-  return 1 if not => airflow step to fail
 
-run_step = issue_step(
-    app_name,
-    ["bash","-c","hdfs dfs -ls /tw/stationMart/data | tr -s ' ' | cut -d' ' -f6-7 | grep '^[0-9]'"])
-
-add_step_task = EmrAddStepsOperator(
-    task_id='add_step',
-    job_flow_id=cluster_id,
-    aws_conn_id='aws_kmok',
-    steps=run_step,
-    dag=dag
+modified_in_last_5mins = PythonOperator(
+   task_id='is_5_mins_ago',
+   python_callable=some_function(),
+   dag=dag,
 )
 
-watch_prev_step_task = EmrStepSensor(
-    task_id='watch_prev_step',
-    job_flow_id=cluster_id,
-    step_id="{{ task_instance.xcom_pull('add_step', key='return_value')[0] }}",
-    aws_conn_id='aws_kmok',
-    dag=dag
-)
-
-add_step_task >> watch_prev_step_task
-
-# check_output = BashOperator(
-#   task_id='check_output',
-#   bash_command='echo "{{ task_instance.xcom_pull("add_step_task")}}"',
-#   dag=dag,
-# )
-
-
-# python operator => compare
-#def is_5_min_ago_command(datetime_string):
-#  datetime.strptime(datetime_string, '%d/%m/%y %H:%M:%S') - datetime.timedelta(minutes=5)
-
-
-#PythonOperator(
-#    task_id='is_5_mins_ago',
-#    python_callable=is_5_min_ago_command("{{ task_instance.xcom_pull('last_timestamp')}}"),
-#    dag=dag,
-#)
-
-
-
-# last_timestamp >> check_output
-#last_timestamp >> watch_prev_step_task
+modified_in_last_5mins
