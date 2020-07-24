cf5356a35f1b6ed0be49d4e1648455d55a50bb9f
kmok <kmok@thoughtworks.com>
2020-07-24 14:40:00 +0200

    km: removed commented out code

--- dags/awsHelpers.py
+++ dags/awsHelpers.py
@@ -7,7 +7,6 @@ def assume_role(arn, session_name):
   print(account_id)
 
   response = client.assume_role(RoleArn=arn, RoleSessionName=session_name)
-  # response = client.assume_role(RoleArn="arn:aws:iam::534731679169:role/stevethepug-test", RoleSessionName="airflow-monitor")
 
   Session(aws_access_key_id=response['Credentials']['AccessKeyId'],
           aws_secret_access_key=response['Credentials']['SecretAccessKey'],
