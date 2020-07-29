29641e147df8880212715af5220ec0eed5815101
kmok <kmok@thoughtworks.com>
2020-07-29 16:28:36 +0200

    km: adding cloudwatch alarms for wrong applications in streaming queue

--- modules/training_emr_cluster/cloudwatch.tf
+++ modules/training_emr_cluster/cloudwatch.tf
@@ -36,3 +36,22 @@ resource "aws_cloudwatch_metric_alarm" "kafka-consumer-running" {
     ConsumerName = "${var.kafka_consumers_to_monitor[count.index]}"
   }
 }
+
+resource "aws_cloudwatch_metric_alarm" "correct-applications-in-streaming-queue" {
+  alarm_name                = "CorrectApplicationsInStreamingQueue"
+  comparison_operator       = "LessThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "yarnApplicationRunningInCorrectQueue"
+  namespace                 = "Custom"
+  period                    = "300"
+  statistic                 = "Maximum"
+  threshold                 = "0.5"
+  alarm_description         = "This metric sends alarm when there is an unexpected Yarn application in the streaming queue"
+  datapoints_to_alarm       = "1"
+  insufficient_data_actions = []
+  ok_actions = ["${var.alerting_sns_topic}"]
+  alarm_actions = ["${var.alerting_sns_topic}"]
+  dimensions = {
+    QueueName = "streaming"
+  }
+}
